cac988f8768d35964485f19d008cd2887f4070ed
BenyaminMusavi <beni97d@gmail.com>
2024-01-01 12:20:03 +0330

    aaa

--- Marina.UI/Controllers/HomeController.cs
+++ Marina.UI/Controllers/HomeController.cs
@@ -18,34 +18,11 @@ public class HomeController : Controller
         _importRepository = importRepository;
     }
 
-    //public IActionResult Index(int? page)
-    //{
-    //    var dbName = Helper.SetNameDb();
-
-    //    var model = _importRepository.GetAll();
-    //    List<DataRow> rows = model.AsEnumerable().ToList();
-
-    //    var pageNumber = page ?? 1;
-    //    int pageSize = 25;
-
-    //    var result = rows.ToPagedList(pageNumber, pageSize);
-    //    return View(result);
-    //}
-
     [Authorize]
     public async Task<IActionResult> Index(int? page, int pageSize = 25)
     {
         var model = await _importRepository.GetAll();
         List<DataRow> rows = model.AsEnumerable().ToList();
-
-        //if (TempData["DataRows"] != null && TempData["DataRows"] is List<DataRow> tempRows && tempRows.SequenceEqual(rows))
-        //{
-        //    rows = tempRows;
-        //}
-        //else
-        //{
-        //    TempData["DataRows"] = rows;
-        //}
 
         var pageNumber = page ?? 1;
 
